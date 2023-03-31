9cbe620e2c4ccbdb490cdd24dc0573b1b9cf43d3
fzy <fzy@gonsin.cn>
2023-03-31 14:22:53 +0800

    feat(api-gateway): ✨ 使用编程式实现路由转发

--- api-gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,8 +1,10 @@
 package com.example.gateway;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author feng
@@ -14,4 +16,14 @@ public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //基于路径配置，访问/provider 路径时会转发到 http://localhost:8083
+                .route("path_route", r -> r.path("/provider/**")
+                        .uri("http://localhost:8083"))
+                .build();
+    }
+
 }
